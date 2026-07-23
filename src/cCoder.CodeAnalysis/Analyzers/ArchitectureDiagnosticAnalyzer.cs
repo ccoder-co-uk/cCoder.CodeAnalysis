443e8dab022b86c8cbfe81ada9bc52a27a734d71
Paul Ward <Paul.Ward@ccoder.co.uk>
2026-07-23 13:53:59 +0100

    Keep consumer-rule fix scoped

--- src/cCoder.CodeAnalysis/Analyzers/ArchitectureDiagnosticAnalyzer.cs
+++ src/cCoder.CodeAnalysis/Analyzers/ArchitectureDiagnosticAnalyzer.cs
@@ -105,19 +105,12 @@ public sealed class ArchitectureDiagnosticAnalyzer : DiagnosticAnalyzer
                     DiagnosticSeverity.Warning,
                     true,
                     null,
-                    GetHelpLinkUri(code)
+                    null
                 ),
             StringComparer.Ordinal
         );
 
     public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => Descriptors.Values.ToImmutableArray();
-
-    private static string GetHelpLinkUri(string code)
-    {
-        string prefix = new string(code.TakeWhile(character => !char.IsDigit(character)).ToArray());
-
-        return $"https://ccoder.co.uk/Documentation/CodeAnalysis/{prefix}/{code}";
-    }
 
     public override void Initialize(AnalysisContext context)
     {
