bbb80d0b5473acf95fd49ab645ff7991949e1144
Paul Ward <Paul.Ward@ccoder.co.uk>
2026-07-23 10:30:54 +0100

    Tighten compatibility classification boundaries

--- src/cCoder.CodeAnalysis.Sample/Exposures/EventProvider.cs
+++ src/cCoder.CodeAnalysis.Sample/Exposures/EventProvider.cs
@@ -2,9 +2,9 @@
 // Copyright (c) Paul.Ward@ccoder.co.uk
 // ---------------------------------------------------------------
 
-namespace cCoder.CodeAnalysis.Sample.Models;
+namespace cCoder.CodeAnalysis.Sample.Models.Compatibility;
 
-internal sealed class EventProviderCompatibility
+internal sealed class EventProvider
 {
     internal void Invoke()
     {
