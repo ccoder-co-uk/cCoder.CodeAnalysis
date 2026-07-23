f22120bc91d2bd8e0d6019ea0ea3c67999594700
Paul Ward <Paul.Ward@ccoder.co.uk>
2026-07-23 14:42:49 +0100

    Exempt external dependencies from standard rules

--- src/cCoder.CodeAnalysis.Sample/Dependencies/CompatibilityDependency.cs
+++ src/cCoder.CodeAnalysis.Sample/Dependencies/CompatibilityDependency.cs
@@ -6,6 +6,20 @@ namespace cCoder.CodeAnalysis.Sample.Dependencies;
 
 internal sealed class CompatibilityDependency
 {
+    private bool lastValue;
+
+    public bool Resolve(bool value)
+    {
+        lastValue = value;
+
+        if (value)
+        {
+            return true;
+        }
+
+        return lastValue;
+    }
+
     internal void Invoke()
     {
     }
