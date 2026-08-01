99687ff4adfde926ab158bbc4ad4e6145aa4d640
Paul Ward <Paul.Ward@ccoder.co.uk>
2026-08-01 12:08:31 +0100

    Isolate mixed-ending sample violation

--- src/cCoder.CodeAnalysis.Sample/Exposures/RuleViolations/AuthorizationExtensions.cs
+++ src/cCoder.CodeAnalysis.Sample/Exposures/RuleViolations/AuthorizationExtensions.cs
@@ -4,7 +4,6 @@
 
 namespace cCoder.CodeAnalysis.Sample.Exposures.RuleViolations;
 
-// Mixed line endings are intentional: this fixture represents STXFORMAT013.
 internal static class AuthorizationExtensions
 {
     internal static void Authorize(object user)
